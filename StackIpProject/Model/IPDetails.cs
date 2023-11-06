e4f64419abe66617814e99793280a5670853cc51
annakitou <anna.kitou@nb.org>
2023-11-06 13:16:19 +0200

    Setting Up The Library - Phase 4

--- StackIpProject/Model/IPDetails.cs
+++ StackIpProject/Model/IPDetails.cs
@@ -7,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace StackIpProject.Model
 {
-    public class IPDetails:IIPDetails
+    public class IPDetails : IIPDetails
     {
+        public int Id { get; set; }
+        public string? Ip { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
